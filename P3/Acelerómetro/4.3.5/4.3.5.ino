fb67a95e7fca6bd305583254c0738db3d3b6f26d
Israel <ifonsecap351@gmail.com>
2021-03-04 21:12:17 -0600

    P3

--- P3/Acelerómetro/4.3.5/4.3.5.ino
+++ P3/Acelerómetro/4.3.5/4.3.5.ino
@@ -1,9 +1,12 @@
+#include <LiquidCrystal.h>  // include the library code:
+LiquidCrystal lcd(12, 11, 5, 4, 3, 2);// initialize the interface pins
 int xpin=A0;
 int ypin=A1;
 int zpin=A2;
 
 void setup() {
   Serial.begin(9600);
+  lcd.begin(16, 2);
 }
 
 void loop() {
@@ -25,13 +28,25 @@ void loop() {
 
   float  roll = ( ( (atan2(Yg,Zg) * 180) / 3.14 ) + 180 );
   float pitch = ( ( (atan2(Zg,Xg) * 180) / 3.14 ) + 180 );
-
+  
   Serial.print("Roll = ");
   Serial.print(roll);
   Serial.print("\t");
   Serial.print("Pitch = ");
   Serial.print(pitch);
+
+  float aux=sqrt(pow(Xg,2)+pow(Yg,2));
+  
+  float angulo = atan2(aux,Zg)*180/PI;
+  
+  lcd.setCursor(1, 0);
+  lcd.print("Angulo ");
+  lcd.print(angulo);
+
+  Serial.print("\t");
+  Serial.print("Angulo = ");
+  Serial.print(angulo);
   Serial.print("\n\n");
+
   delay(500);
- 
 }
--- P3/SensorTemperatura/4.2/4.2.ino
+++ P3/SensorTemperatura/4.2/4.2.ino
@@ -1,6 +1,6 @@
 #include <LiquidCrystal.h>  // include the library code:
 LiquidCrystal lcd(12, 11, 5, 4, 3, 2);// initialize the interface pins
-int analogPin0 = A3;
+int analogPin0 = A0;
 int lect=0;
 float volt=0;
 float C=0;
@@ -11,20 +11,17 @@ void setup() {
 
 void loop() {
   lect=analogRead(analogPin0);
-
-  C=((float(lect)*205)/1023)-55; //Usando las especificaciones
-
-  volt=((float(lect)*16)/1023)+4;
-
-  //C=((float(lect)*50)/1023?); //Usando las 50°C como 500mv
-
-  //volt=((float(lect)*5)/1023?);
+  
+  volt=((float(lect)*5)/1023);
+  
+  C=volt/.01;
     
-  lcd.setCursor(1, 0);
-  lcd.print("Temperatura: ");
+  lcd.setCursor(0, 0);
+  lcd.print("Temperatura ");
   lcd.print(C);
+  
 
-  lcd.setCursor(1, 1);
+  lcd.setCursor(0, 1);
   lcd.print("Voltaje: ");
   lcd.print(volt);
 }
